23a069e37c6f650cd70845b6d9f351306989cf15
Poliack <you@example.com>
2018-02-22 13:06:22 +0100

    MAJ TWIG

---
+++ As_football/app/Controllers/PageController.php
@@ -0,0 +1,16 @@
+<?php
+namespace App\Controllers;
+
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+
+Class PageController{
+    private $container;
+    public function __construct($container)
+    {
+        $this->container=$container;
+    }
+    public function home(RequestInterface $request,ResponseInterface $response){
+        $this->container->view->render($response,'pages/home.twig');
+    }
+}
